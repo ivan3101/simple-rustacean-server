6f8a9a659a179e4c100b252501ebf80190607e9a
Ivan De Menezes <ivandmenezes@gmail.com>
2021-10-20 00:45:18 +0200

    feat: add server boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new(String::from("127.0.0.1:8000"));
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    fn run(&self) {
+        println!("Server listening on: {}", self.addr)
+    }
 }
